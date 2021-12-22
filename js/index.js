c8beb8f874054159850f27fa04d8f52ba3a40d60
na-pydev <nurmatov.abdurahim98@mail.ru>
2021-12-22 20:20:15 +0500

    'improved'

--- js/index.js
+++ js/index.js
@@ -106,7 +106,7 @@ function choice(event) {
     console.log(trueAnswers)
     if (trueAnswers == cardAmount) {
       level++;
-      setTimeout(function () { showMessage('Keyingi bosqich sizni kutmoqda!', 'rgba(25, 135, 84, 0.8)', 'fa-play', 'nextLevel()') }, 1000);
+      setTimeout(function () { showMessage(`Congrats!<br/>Next level`, 'rgba(25, 135, 84, 0.8)', 'fa-play', 'nextLevel()') }, 1000);
     }
   }
   else {
@@ -116,7 +116,7 @@ function choice(event) {
     trueAnswers = 0;
     let result = level;
     chance = 4;
-    setTimeout(function () { showMessage(`Siz natijangiz <br>${result}-bosqich`, '#dc3848cc', 'fa-refresh', 'start()') }, 1000);
+    setTimeout(function () { showMessage(`Your result <br>${result}-level`, '#dc3848cc', 'fa-refresh', 'start()') }, 1000);
     level = 1;
     document.getElementById('level').innerHTML = level;
   }
